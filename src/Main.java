882ed4c9cceb94143504d8b70a7778fa9261d4eb
Алекс <dendyboy777@gmail.com>
2022-08-14 12:53:07 +0300

    wh4-2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        // задание 1
        for (int i=10; i>0; i--){
            System.out.println(i);
        }
@@ -8,4 +8,8 @@ public class Main {
        while (a<10) {
            a++;
            System.out.println(a);
-       }}}
+       }
+       // задание 2
+ int w =7;
+       while (w<31) {  w=w+7;
+        System.out.println("Cегодня пятница " +w +" число. Необходимо подготовить отчет.");}}}
