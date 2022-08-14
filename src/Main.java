aeb7c7545dd23130328c6fca93c933a1f2605f7c
Алекс <dendyboy777@gmail.com>
2022-08-14 12:24:50 +0300

    wh4-1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-}}
+       for (int i=10; i>0; i--){
+           System.out.println(i);
+       }
+       int a =0;
+       while (a<10) {
+           a++;
+           System.out.println(a);
+       }}}
