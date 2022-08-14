162c42c17e6d627d8ae2cbc2158ca47faf0bb1a3
Алекс <dendyboy777@gmail.com>
2022-08-14 12:59:30 +0300

    wh4-3

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,15 @@ public class Main {
     public static void main(String[] args) {
         // задание 1
        for (int i=10; i>0; i--){
-           System.out.println(i);
+           System.out.print(i + " ");
        }
        int a =0;
+        System.out.println();
        while (a<10) {
            a++;
-           System.out.println(a);
+           System.out.print(a+" ");
        }
+        System.out.println();
        // задание 2
  int w =7;
        while (w<31) {  w=w+7;
